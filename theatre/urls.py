7b582dd1f7663cece6d26da1d545cdc135cbdfff
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-08-08 18:28:01 +0300

    Add path to GenreViewSet, ActorViewSet, PlayViewSet

---
+++ theatre/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from rest_framework import routers
+
+from theatre.views import GenreViewSet, ActorViewSet, PlayViewSet
+
+router = routers.DefaultRouter()
+router.register("genres", GenreViewSet)
+router.register("actors", ActorViewSet)
+router.register("plays", PlayViewSet)
+
+urlpatterns = [path("", include(router.urls))]
+
+app_name = "theatre"
