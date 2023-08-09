bf0d6569ed5fd1f65435de585acaa03474ba55b4
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-08-09 14:15:39 +0300

    Add functional to create Ticket inside Reservation

--- theatre/admin.py
+++ theatre/admin.py
@@ -2,10 +2,20 @@ from django.contrib import admin
 
 from theatre.models import Genre, Actor, Play, TheatreHall, Performance, Ticket, Reservation
 
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Reservation)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
 admin.site.register(Genre)
 admin.site.register(Actor)
 admin.site.register(Play)
 admin.site.register(TheatreHall)
 admin.site.register(Performance)
 admin.site.register(Ticket)
-admin.site.register(Reservation)
