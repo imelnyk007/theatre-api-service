4a79089b2a2c654562592a4d3b91733a05033c0a
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-08-10 20:41:54 +0300

    Set up .env

--- theatre_api_service/settings.py
+++ theatre_api_service/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-7_qm*6)-+9u-0)*l+ikihu!gzn*h*gh$++(!9w&zsmiz=(-cou"
+SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 DEBUG = True
